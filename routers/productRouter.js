1c4dc9d1e525c8d3c045594cbf1303827a795182
Mrunal Bele <mb725744@gmail.com>
2021-06-14 00:15:21 +0530

    6

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import data from "../data.js"
 import expressAsyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 const productRouter = express.Router()
@@ -7,12 +6,6 @@ const productRouter = express.Router()
 productRouter.get('/',expressAsyncHandler(async(req,res)=>{
     const products = await Product.find({})
     res.send(products)
-}))
-
-productRouter.get('/seed', expressAsyncHandler(async(req,res)=>{
-    await Product.remove({})
-    const createdProducts = await Product.insertMany(data.products)
-    res.send({createdProducts})
 }))
 
 productRouter.get('/:id',expressAsyncHandler(async(req,res)=>{
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,16 +1,9 @@
 import express from 'express'
-import data from "../data.js"
 import User from '../models/userModel.js'
 import expressAsyncHandler from 'express-async-handler'
 import bcrypt from 'bcryptjs'
 import { generateToken, isAuth } from '../utils.js'
 const userRouter = express.Router()
-
-userRouter.get('/seed', expressAsyncHandler(async(req,res)=>{
-    await User.remove({})
-    const createdUsers = await User.insertMany(data.users)
-    res.send({createdUsers})
-}))
 
 userRouter.post('/signin',expressAsyncHandler(async(req,res)=>{
     const user = await User.findOne({email: req.body.email})
