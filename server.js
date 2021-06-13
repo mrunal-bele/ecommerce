87817a12959971245f1bd5e6722ea7efff7aacb8
Mrunal Bele <mb725744@gmail.com>
2021-06-13 18:22:06 +0530

    v1.0.1

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((err, req, res, next) => {
     res.status(500).send({message: err.message})
 });
 
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) 
